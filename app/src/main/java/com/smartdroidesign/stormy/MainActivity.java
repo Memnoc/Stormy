051da306678ab663131115a1c7ad754a9321a28b
Memnoc <smartdroidesign@gmail.com>
2018-09-26 13:38:09 +0100

    created a model - wired to the API service

--- app/src/main/java/com/smartdroidesign/stormy/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/stormy/MainActivity.java
@@ -1,8 +1,15 @@
 package com.smartdroidesign.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private CurrentWeather currentWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,39 +37,96 @@ public class MainActivity extends AppCompatActivity {
         String foreCastURL = "https://api.darksky.net/forecast/"
                 + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url(foreCastURL)
-                .build();
+            Request request = new Request.Builder()
+                    .url(foreCastURL)
+                    .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if (response.isSuccessful()) {
-                    } else {
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "IO Exception caught: ", e);
                 }
-            }
-        });
 
-        Log.d(TAG, "Main UI code is running");
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        String jsonData = null;
+                        if (response.body() != null) {
+                            jsonData = response.body().string();
+                        }
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            currentWeather = getCurrentDetails(jsonData);
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "JSON Exception caught: ", e);
+                    }
+                }
+            });
 
+            Log.d(TAG, "Main UI code is running");
+        }
+    }
+
+
+    public CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+
+        String timezone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setLocationLabel("Alcatraz Island, CA");
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+
+//        Log.i(TAG, "from JSON: " + timezone + currently);
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (manager != null) {
+            networkInfo = manager.getActiveNetworkInfo();
+        }
+
+        boolean isAvailable = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        } else {
+            alertUserAboutNetworkError();
+//            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+        return isAvailable;
     }
 
     private void alertUserAboutError() {
         AlertDialogFragment dialog =  new AlertDialogFragment();
         dialog.show(getSupportFragmentManager(), "error_dialog");
+    }
+
+
+    private void alertUserAboutNetworkError() {
+        NetworkAlertDialogFragment networkDialog = new NetworkAlertDialogFragment();
+        networkDialog.show(getSupportFragmentManager(), "error_dialog");
     }
 }
--- app/src/main/java/com/smartdroidesign/stormy/NetworkAlertDialogFragment.java
+++ app/src/main/java/com/smartdroidesign/stormy/NetworkAlertDialogFragment.java
@@ -5,21 +5,19 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatDialogFragment;
 
-public class AlertDialogFragment extends AppCompatDialogFragment {
+public class NetworkAlertDialogFragment extends AppCompatDialogFragment {
     @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         Context context = getActivity();
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
-        builder.setTitle(R.string.error_title)
-        .setMessage(R.string.error_message)
-        .setPositiveButton(R.string.error_button_ok_text, null);
+        builder.setTitle(R.string.error_network)
+                .setMessage(R.string.error_network_unavailable)
+                .setPositiveButton(R.string.error_button_ok_text, null);
 
         return builder.create();
-
     }
 }
